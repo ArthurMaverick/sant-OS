2e672dcebeef68ecde1f0761bb6b942d5a9cbd5c
ArthurMaverick <arthuracs18@gmail.com>
2021-08-19 15:03:28 -0300

    create a package in master dir for dependecies

--- frontend/src/container/post/index.tsx
+++ frontend/src/container/post/index.tsx
@@ -1,25 +1,27 @@
-import Head from 'next/head'
-import { Header,MainContainer, Heading, DateDetails, PostContainer} from '../../components'
+import Head from 'next/head';
+import {
+  Header, MainContainer, Heading, DateDetails, PostContainer,
+} from '../../components';
 
-export const Post = ({post}: any) => {
+export const Post = ({ post }: any) => {
   return (
     <>
-      <Head>
-        <title>{post.title}</title>
-        <meta name="sant-OS" content={post.title}/>
-      </Head>
-      <Header/>
+    <Head>
+      <title>{post.title}</title>
+      <meta name="sant-OS" content={post.title} />
+    </Head>
+    <Header />
 
-      <MainContainer>
-        <Heading>
-         {post.title} 
-        </Heading>
+    <MainContainer>
+      <Heading>
+        {post.title}
+      </Heading>
 
-        <DateDetails date={post.createdAt}/>
-          
-        <PostContainer content={post.body}/>
+      <DateDetails date={post.createdAt} />
 
-      </MainContainer>
-    </>
+      <PostContainer content={post.body} />
+
+    </MainContainer>
+  </>
   )
-}
+  };
